061ad4c62588527ef981acc1cbbd65a2eb5640b1
Edd Barrett <vext01@gmail.com>
2020-08-19 14:16:39 +0100

    Initial tracing of ykbf.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(yk)]
+
 use std::{
     env,
     ffi::CString,
@@ -6,6 +8,9 @@ use std::{
     path::Path,
     process,
 };
+
+use ykcompile::TraceCompiler;
+use yktrace::{start_tracing, tir::TirTrace, TracingKind};
 
 use getopts::Options;
 use libc::{self, c_int, fdopen, ferror, getchar, putchar};
@@ -51,63 +56,81 @@ fn compile(txt: &str) -> (String, Vec<usize>) {
 }
 
 fn interp(prg: &[u8], map: Vec<usize>) {
-    let mut pc = 0;
-    let mut ptr = 0;
-    let mut cells = Vec::with_capacity(30000);
-    cells.resize(30000, 0u8);
     let stdin = {
         let mode = CString::new("r").unwrap();
         unsafe { fdopen(libc::STDIN_FILENO, mode.as_ptr()) }
     };
-    while pc < prg.len() {
-        match prg[pc] as char {
-            '>' => {
-                if ptr == cells.len() {
-                    cells.push(0);
-                }
-                ptr += 1;
-            }
-            '<' => {
-                if ptr > 0 {
-                    ptr -= 1;
-                }
-            }
-            '+' => {
-                cells[ptr] = cells[ptr].wrapping_add(1);
-            }
-            '-' => {
-                cells[ptr] = cells[ptr].wrapping_sub(1);
-            }
-            '.' => {
-                let b = cells[ptr];
-                unsafe {
-                    putchar(b as c_int);
-                }
-            }
-            ',' => {
-                let v = unsafe { getchar() };
-                if v == libc::EOF {
-                    if unsafe { ferror(stdin) } != 0 {
-                        panic!("Error when reading from stdin.");
-                    }
-                } else {
-                    cells[ptr] = v as u8;
-                }
-            }
-            '[' => {
-                if cells[ptr] == 0 {
-                    pc = map[pc];
-                }
-            }
-            ']' => {
-                if cells[ptr] != 0 {
-                    pc = map[pc];
-                }
-            }
-            _ => unreachable!(),
-        }
-        pc += 1;
+
+    let pc = 0;
+    let ptr = 0;
+    let mut cells = Vec::with_capacity(30000);
+    cells.resize(30000, 0u8);
+    let mut tio = core::yk::trace_inputs((pc, ptr, cells));
+
+    while tio.0 < prg.len() {
+        let tr = start_tracing(Some(TracingKind::HardwareTracing));
+        interp_inner(prg, map, stdin, &mut tio);
+        let sir_trace = tr.stop_tracing().unwrap();
+
+        let tir_trace = TirTrace::new(&*sir_trace).unwrap();
+        let _comp_trace = TraceCompiler::<&(usize, usize, Vec<u8>)>::compile(tir_trace);
+        todo!("execute the trace");
     }
+}
+
+fn interp_inner(
+    prg: &[u8],
+    map: Vec<usize>,
+    stdin: *mut libc::FILE,
+    tio: &mut (usize, usize, Vec<u8>),
+) {
+    match prg[tio.0] as char {
+        '>' => {
+            if tio.1 == tio.2.len() {
+                tio.2.push(0);
+            }
+            tio.1 += 1;
+        }
+        '<' => {
+            if tio.1 > 0 {
+                tio.1 -= 1;
+            }
+        }
+        '+' => {
+            tio.2[tio.1] = tio.2[tio.1].wrapping_add(1);
+        }
+        '-' => {
+            tio.2[tio.1] = tio.2[tio.1].wrapping_sub(1);
+        }
+        '.' => {
+            let b = tio.2[tio.1];
+            unsafe {
+                putchar(b as c_int);
+            }
+        }
+        ',' => {
+            let v = unsafe { getchar() };
+            if v == libc::EOF {
+                if unsafe { ferror(stdin) } != 0 {
+                    panic!("Error when reading from stdin.");
+                }
+            } else {
+                tio.2[tio.1] = v as u8;
+            }
+        }
+        '[' => {
+            if tio.2[tio.1] == 0 {
+                tio.0 = map[tio.0];
+            }
+        }
+        ']' => {
+            if tio.2[tio.1] != 0 {
+                tio.0 = map[tio.0];
+            }
+        }
+        _ => unreachable!(),
+    }
+    tio.0 += 1;
 }
 
 fn main() {
